64289c3066eb08f6324e57f3f36eb69757486c5a
ar239 <ar239@host.docker.internal>
2020-09-18 16:28:11 +0530

    commit

--- Registration/src/test/java/com/MusicApp/Registration/RegistrationApplicationTests.java
+++ Registration/src/test/java/com/MusicApp/Registration/RegistrationApplicationTests.java
@@ -1,13 +1,12 @@
 package com.MusicApp.Registration;
 
 
-import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RegistrationApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
